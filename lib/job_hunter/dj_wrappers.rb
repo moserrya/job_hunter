477c390c2cabf480ebb7842f97cf933bdbfe681a
Ryan Moser <ryanpmoser@gmail.com>
2014-05-10 19:32:02 -0700

    [FEATURE] add .find to DJ wrappers

--- lib/job_hunter/dj_wrappers.rb
+++ lib/job_hunter/dj_wrappers.rb
@@ -3,4 +3,9 @@ module JobHunter
     options = args.last.is_a?(Hash) ? args.pop : {}
     Delayed::Job.enqueue(new(*args), options)
   end
+
+  def find(*args)
+    handler = new(*args).to_yaml
+    Delayed::Job.where(handler: handler).first
+  end
 end
