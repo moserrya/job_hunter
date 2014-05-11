0651f49ee7ba4bb813ad5207f4c9e8bf37a9d308
Ryan Moser <ryanpmoser@gmail.com>
2014-05-10 20:12:46 -0700

    [SPEC] add tests for .find_or_create

--- spec/job_hunter_spec.rb
+++ spec/job_hunter_spec.rb
@@ -11,9 +11,10 @@ end
 describe JobHunter do
   let(:model_id) { 47 }
   let(:details)  { 'Party Solver' }
-  let(:options)  { {run_at: 5.hours.from_now} }
+  let(:run_at)   { DateTime.parse('January 28, 2015 0800') }
+  let(:options)  { { run_at: run_at } }
 
-  context '#create' do
+  context '.create' do
     it 'enqueues Delayed Jobs' do
       expect {
         KustomJob.create model_id, details
@@ -33,7 +34,7 @@ describe JobHunter do
     end
   end
 
-  context '#find' do
+  context '.find' do
     before do
       custom_job = KustomJob.new model_id, details
       @job = Delayed::Job.enqueue custom_job
@@ -41,6 +42,30 @@ describe JobHunter do
 
     it 'finds the job given the same arguments' do
       expect(KustomJob.find(model_id, details)).to eq(@job)
+    end
+  end
+
+  context '.find_or_create' do
+    it 'creates the job when it is not in the queue' do
+      expect {
+        KustomJob.find_or_create model_id, details, options
+      }.to change{Delayed::Job.count}.by(1)
+    end
+
+    context 'job already enqueued' do
+      before do 
+        @job = KustomJob.create model_id, details
+      end
+
+      it 'does not enqueue a job if one with the same arguments was already enqueued' do
+        expect {
+          KustomJob.find_or_create model_id, details, options
+        }.not_to change{Delayed::Job.count}
+      end
+
+      it 'returns the job if it was already enqueued' do
+        expect(KustomJob.find_or_create model_id, details, options).to eq(@job)
+      end
     end
   end
 end
