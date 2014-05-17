4e58155423bf8d4349823cdab72237bff643500b
Ryan Moser <ryanpmoser@gmail.com>
2014-05-16 22:09:37 -0700

    [FEATURE] add extend_or_create

--- lib/job_hunter/dj_wrappers.rb
+++ lib/job_hunter/dj_wrappers.rb
@@ -1,31 +1,6 @@
 module JobHunter
-  def self.extended(klass)
-    klass.include(InstanceMethods)
-  end
-
-  def create(*args)
-    new_with_options(*args).create
-  end
-
-  def find(*args)
-    new_with_options(*args).find
-  end
-
-  def find_or_create(*args)
-    new_with_options(*args).find_or_create
-  end
-
-  def destroy(*args)
-    new_with_options(*args).destroy
-  end
-
-  def new_with_options(*args)
-    options = args.extract_options!
-    job = new(*args)
-    job.define_singleton_method(:_options_) do
-      self.class._defaults_.merge(options)
-    end
-    job
+  def self.extended(base_class)
+    base_class.include(InstanceMethods)
   end
 
   module InstanceMethods
@@ -45,9 +20,34 @@ module JobHunter
       find.try(:destroy)
     end
 
+    def extend_or_create
+      if job = find
+        if run_at = self.class._defaults_[:run_at]
+          job.update_attributes run_at: run_at
+        end
+      else
+        create
+      end
+    end
+
     private
     def _options_
       {}
     end
+  end
+
+  InstanceMethods.public_instance_methods.each do |method_sym|
+    define_method(method_sym) do |*args|
+      new_with_options(*args).send method_sym
+    end
+  end
+
+  def new_with_options(*args)
+    options = args.extract_options!
+    job = new(*args)
+    job.define_singleton_method(:_options_) do
+      self.class._defaults_.merge(options)
+    end
+    job
   end
 end
